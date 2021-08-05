6baceb9d87418f77b0fa1381455d591ad8893fed
Morgan Creekmore <morgan@creekmore.email>
2021-08-05 00:08:01 -0500

    Add debug controller

---
+++ src/Skateboard3Server.Qos/Controllers/DebugController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Skateboard3Server.Qos.Controllers
+{
+    /// <summary>
+    /// Not a real controller used by skate, used by admin or other services
+    /// </summary>
+    [Route("/debug")]
+    [ApiController]
+    public class DebugController : ControllerBase
+    {
+        [HttpGet("status")]
+        public StatusUpdate Status()
+        {
+            var current = System.Diagnostics.Process.GetCurrentProcess();
+            return new StatusUpdate
+            {
+                StartTime = current.StartTime
+            };
+        }
+    }
+
+    public class StatusUpdate
+    {
+        //TODO: maybe connected users?
+        public DateTime StartTime { get; set; }
+    }
+}
